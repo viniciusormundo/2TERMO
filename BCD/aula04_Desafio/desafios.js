2611d9e450cb5b860928d466de409e08acb22c4b
viniciusormundo <viniciusormundo8@gmail.com>
2026-08-14 10:03:22 -0300

    ai

--- BCD/aula04_Desafio/desafios.js
+++ BCD/aula04_Desafio/desafios.js
@@ -2,9 +2,9 @@ const entradas = require('readline-sync');
 
 console.log('--- controle de acesso ---')
 const login = entradas.questionInt('Digite sua idade: ');
-const autorizacao = entradas.question('Você tem autorização? (s/n): ') === 's';
-const professor = entradas.question('Você é professor? (s/n): ') === 's';
-const suepenso = entradas.question('Você está suspenso? (s/n): ') === 'n';
+const autorizacao = entradas.question('Você tem autorização? (s/n): ') === 's'.toUpperCaseCase();
+const professor = entradas.question('Você é professor? (s/n): ') === 's'.toUpperCaseCase();
+const suepenso = entradas.question('Você está suspenso? (s/n): ') === 'n'.toUpperCaseCase();
 
 
 if ((autorizacao && login >= 16) || professor) {
