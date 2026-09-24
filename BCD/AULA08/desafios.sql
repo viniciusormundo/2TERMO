5a61049dac240bcac9374aa20eb0f4f1cf195ffe
viniciusormundo <viniciusormundo8@gmail.com>
2026-09-24 13:24:43 -0300

    mit sql

--- BCD/AULA08/desafios.sql
+++ BCD/AULA08/desafios.sql
@@ -12,7 +12,7 @@ VALUES ('Especiais da Casa');
 -- 3.
 INSERT INTO produto (nome, preco, id_categoria, ativo) 
 VALUES 
-    ('Ratatouille 1', 45.00, LAST_INSERT_ID(), TRUE),
+    ('Ratatouille', 45.00, LAST_INSERT_ID(), TRUE),
     ('Hamburguer de siri', 55.50, LAST_INSERT_ID(), TRUE),
     ('bolinhos da Tiana"', 22.00, LAST_INSERT_ID(), TRUE);
 
